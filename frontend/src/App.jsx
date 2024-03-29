4c88af0f0025c7ca5bf7a3467d2fa392b038fd3a
Nisson <nisson2000@gmail.com>
2024-03-29 15:56:32 +0800

    page_6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import UserPage from './pages/UserPage';
